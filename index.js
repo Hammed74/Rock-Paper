ad3162e88cbccc80cd3d745ccc43cff776e429f3
Rarri74 <Hammed74@gmail.com>
2023-10-18 14:41:02 -0400

    added more styling elements,currently bug in logic

--- index.js
+++ index.js
@@ -1,116 +1,124 @@
-const rock = document.querySelector(".rock")
-const paper = document.querySelector(".paper")
-const scissors = document.querySelector(".scissors")
-const gameText = document.querySelector(".verdict")
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const gameText = document.querySelector(".verdict");
+const userSelection = document.querySelector(".user-choice")
+const cpuSelection = document.querySelector(".computer-choice");
 
 document.body.appendChild(gameText);
 
-
-function getComputerChoice(){
-    let computerChoice = (Math.random() * 10);
-    if (computerChoice <= 3.33){
-        return "rock";
-       } else if (computerChoice > 3.33 && computerChoice <= 6.66){
-        return "scissors";
-      } else {
-        return "paper";
-       }
-
+function getComputerChoice() {
+  let computerChoice = Math.random() * 10;
+  if (computerChoice <= 3.33) {
+    return "Rock";
+  } else if (computerChoice > 3.33 && computerChoice <= 6.66) {
+    return "Scissors";
+  } else {
+    return "Paper";
+  }
 }
-rock.addEventListener("click", function(){
-    let verdict = playRoundRock();
-    gameText.textContent = verdict;
-});
-paper.addEventListener("click", function(){
-    let verdict = playRoundPaper();
-    gameText.textContent = verdict;
-});
-scissors.addEventListener("click", function(){
-    let verdict = playRoundScissors();
-    gameText.textContent = verdict;
-});
+rock.addEventListener("click", function () {
+  let verdict = playRoundRock();
+  userSelection.textContent = "You chose: Rock"
+  cpuSelection.textContent = `Computer chose: ${getComputerChoice()}`;
+  gameText.textContent = verdict;
 
 
-function playRoundRock(){
-let userChoice = "rock";
-let computerHand = getComputerChoice();
-if (userChoice == computerHand){
+});
+paper.addEventListener("click", function () {
+  let verdict = playRoundPaper();
+  userSelection.textContent = "You chose: Paper";
+  cpuSelection.textContent = `Computer chose: ${getComputerChoice()}`;
+  gameText.textContent = verdict;
+
+});
+scissors.addEventListener("click", function () {
+  let verdict = playRoundScissors();
+  userSelection.textContent = "You chose: Scissors";
+  cpuSelection.textContent = `Computer chose: ${getComputerChoice()}`;
+  gameText.textContent = verdict;
+
+});
+
+function playRoundRock() {
+  let userChoice = "Rock";
+  let computerHand = getComputerChoice();
+  if (userChoice == computerHand) {
     return "Draw!";
-} else if ((userChoice === "rock") && (computerHand === "scissors")){
-        return "Congrats! You win, " + userChoice + " beats " + computerHand + "!"
-} else if ((userChoice === "rock") && (computerHand === "paper")){
-        return "Sorry! You lose, " + computerHand + " beats " + userChoice + "!"
-} else if ((userChoice === "paper") && (computerHand === "rock")){
-        return "Congrats! You win, " + userChoice + " beats " + computerHand + "!"
-} else if ((userChoice === "paper") && (computerHand === "scissors")){
-        return "Sorry! You lose, " + computerHand + " beats " + userChoice + "!"
-} else if ((userChoice === "scissors") && (computerHand === "paper")){
-        return "Congrats! You win, " + userChoice + " beats " + computerHand + "!"
-} else if ((userChoice === "scissors") && (computerHand === "rock")){
-        return "Sorry! You lose, " + computerHand + " beats " + userChoice + "!"
-} 
+  } else if (userChoice === "Rock" && computerHand === "Scissors") {
+    return "YOU WIN!";
+  } else if (userChoice === "Rock" && computerHand === "Paper") {
+    return "YOU LOSE!";
+  } else if (userChoice === "Paper" && computerHand === "Rock") {
+    return "YOU WIN!";
+  } else if (userChoice === "Paper" && computerHand === "Scissors") {
+    return "YOU LOSE!";
+  } else if (userChoice === "Scissors" && computerHand === "Paper") {
+    return "YOU WIN!";
+  } else if (userChoice === "Scissors" && computerHand === "Rock") {
+    return "YOU LOSE!";
+  }
 }
 
-function playRoundPaper(){
-    let userChoice = "paper";
-    let computerHand = getComputerChoice();
-    if (userChoice == computerHand){
-        return "Draw!"
-    } else if ((userChoice === "rock") && (computerHand === "scissors")){
-            return "Congrats! You win, " + userChoice + " beats " + computerHand + "!"
-    } else if ((userChoice === "rock") && (computerHand === "paper")){
-            return "Sorry! You lose, " + computerHand + " beats " + userChoice + "!"
-    } else if ((userChoice === "paper") && (computerHand === "rock")){
-            return "Congrats! You win, " + userChoice + " beats " + computerHand + "!"
-    } else if ((userChoice === "paper") && (computerHand === "scissors")){
-            return "Sorry! You lose, " + computerHand + " beats " + userChoice + "!"
-    } else if ((userChoice === "scissors") && (computerHand === "paper")){
-            return "Congrats! You win, " + userChoice + " beats " + computerHand + "!"
-    } else if ((userChoice === "scissors") && (computerHand === "rock")){
-            return "Sorry! You lose, " + computerHand + " beats " + userChoice + "!"
-    } 
+function playRoundPaper() {
+  let userChoice = "Paper";
+  let computerHand = getComputerChoice();
+  if (userChoice == computerHand) {
+    return "Draw!";
+  } else if (userChoice === "Rock" && computerHand === "Scissors") {
+    return "YOU WIN!";
+  } else if (userChoice === "Rock" && computerHand === "Paper") {
+    return "YOU LOSE!";
+  } else if (userChoice === "Paper" && computerHand === "Rock") {
+    return "YOU WIN!";
+  } else if (userChoice === "Paper" && computerHand === "Scissors") {
+    return "YOU LOSE!";
+  } else if (userChoice === "Scissors" && computerHand === "Paper") {
+    return "YOU WIN!";
+  } else if (userChoice === "Scissors" && computerHand === "Rock") {
+    return "YOU LOSE!";
+  }
+}
+
+function playRoundScissors() {
+  let userChoice = "Scissors";
+  let computerHand = getComputerChoice();
+  if (userChoice == computerHand) {
+    return "Draw!";
+  } else if (userChoice === "Rock" && computerHand === "Scissors") {
+    return "YOU WIN!";
+  } else if (userChoice === "Rock" && computerHand === "Paper") {
+    return "YOU LOSE!";
+  } else if (userChoice === "Paper" && computerHand === "Rock") {
+    return "YOU WIN!";
+  } else if (userChoice === "Paper" && computerHand === "Scissors") {
+    return "YOU LOSE!";
+  } else if (userChoice === "Scissors" && computerHand === "Paper") {
+    return "YOU WIN!";
+  } else if (userChoice === "Scissors" && computerHand === "Rock") {
+    return "YOU LOSE!";
+  }
+}
+
+function game(rounds, playerWins = 0, computerWins = 0) {
+  if (rounds > 0) {
+    let result = playRound();
+    return result;
+
+    if (result.includes("You win")) {
+      playerWins++;
+    } else if (result.includes("You lose")) {
+      computerWins++;
     }
 
-    function playRoundScissors(){
-        let userChoice = "scissors";
-        let computerHand = getComputerChoice();
-        if (userChoice == computerHand){
-            return "Draw!"
-        } else if ((userChoice === "rock") && (computerHand === "scissors")){
-                return "Congrats! You win, " + userChoice + " beats " + computerHand + "!"
-        } else if ((userChoice === "rock") && (computerHand === "paper")){
-                return "Sorry! You lose, " + computerHand + " beats " + userChoice + "!"
-        } else if ((userChoice === "paper") && (computerHand === "rock")){
-                return "Congrats! You win, " + userChoice + " beats " + computerHand + "!"
-        } else if ((userChoice === "paper") && (computerHand === "scissors")){
-                return "Sorry! You lose, " + computerHand + " beats " + userChoice + "!"
-        } else if ((userChoice === "scissors") && (computerHand === "paper")){
-                return "Congrats! You win, " + userChoice + " beats " + computerHand + "!"
-        } else if ((userChoice === "scissors") && (computerHand === "rock")){
-                return "Sorry! You lose, " + computerHand + " beats " + userChoice + "!"
-        } 
-        }
-        
-
-function game(rounds, playerWins = 0, computerWins = 0){
-if (rounds > 0){
-let result = playRound();
-return result;
-
-if (result.includes("You win")) {
-    playerWins++;
-} else if (result.includes("You lose")){
-    computerWins++;
-}
-
-game(rounds - 1, playerWins, computerWins);
-} else {
-if (playerWins > computerWins) {
-    return "You win the game!";
- } else if (playerWins < computerWins){
-    return "Computer wins the game!";
-} else {
-    return "It's a draw!"
-}
-}
+    game(rounds - 1, playerWins, computerWins);
+  } else {
+    if (playerWins > computerWins) {
+      return "You win the game!";
+    } else if (playerWins < computerWins) {
+      return "Computer wins the game!";
+    } else {
+      return "It's a draw!";
+    }
+  }
 }
